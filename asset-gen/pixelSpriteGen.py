b16169be8f4065788de50c14d61094fe5e18779d
dorpg9 <57447480+dorpg9@users.noreply.github.com>
2025-04-19 23:00:42 +1000

    little endian pixel sheet

--- asset-gen/pixelSpriteGen.py
+++ asset-gen/pixelSpriteGen.py
@@ -1,10 +1,12 @@
-import itertools
 from PIL import Image
-import numpy as np
 
-variation44Imgs = [Image.fromarray(np.asarray([list(zip([255]*16, r)) for r in v], dtype=np.dtype('uint8'))) for v in list(itertools.product(list(itertools.product([0,255], repeat=4)), repeat=4))]
+sheet = Image.new("RGBA", (1024,1024), (255, 255, 255, 0))
+for chunk in range(65536):
+    imageRectOffset_x = chunk%256*4
+    imageRectOffset_y = chunk//256*4
 
-sheet = Image.new("LA", (1024,1024), (255,255))
-for i,v in enumerate(variation44Imgs):
-    sheet.paste(v, ((255-i%256)*4, (i//256)*4))
-sheet.save("4x4PixelsInv.png")
+    for bI in range(16):
+        cX, cY = bI%4, bI//4
+        sheet.putpixel((imageRectOffset_x + cX, imageRectOffset_y + cY), (255, 255, 255, ((chunk >> bI) & 1)* 255))
+
+sheet.save("4x4PixelsLittle.png", format="PNG")
